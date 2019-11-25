16e39bf5404ec586a947f283491774f70e54cb88
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-25 17:30:39 +0800

    Fixed typo in dotenv postgres should be postgresql

--- workshops/containers/containers/frontend/entrypoint.sh
+++ workshops/containers/containers/frontend/entrypoint.sh
@@ -7,7 +7,7 @@
 
 # wait for backend api to become available
 echo "waiting for backend to become available..."
-if wait-for $REACT_APP_API_HOST -t 30
+if wait-for $BACKEND_HOST -t 30
 then 
     # run command 
     exec "$@"
