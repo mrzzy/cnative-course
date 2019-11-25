641090b12e3a6fa4c20b61be6d2c4195ccb28ffd
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-25 14:18:34 +0800

    Added entrypoint script to frontend to  wait for api to become available before starting

---
+++ workshops/containers/containers/frontend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Cloud Native/Containers 
+# Workshop - Frontend
+# Container Entrypoint
+#
+
+# wait for backend api to become available
+if wait-for $REACT_APP_API_HOST -t 30
+then 
+    # run command 
+    exec "$@"
+fi
